47a19439bf8b146555963db9be86d46e38f220aa
Seth Nute <sethnute@gmail.com>
2016-03-08 13:03:57 -0500

    Remove unnecessary change

--- mobile/src/main/java/com/teamcasey/watchdog/SettingsActivity.java
+++ mobile/src/main/java/com/teamcasey/watchdog/SettingsActivity.java
@@ -40,19 +40,12 @@ public class SettingsActivity extends PreferenceActivity {
      */
     private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
-    public DatabaseHelper databaseHelper = null;
-
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
         setupSimplePreferencesScreen();
-        loadDatabase();
-    }
-
-    private void loadDatabase() {
-        databaseHelper = new DatabaseHelper(this);
     }
 
     /**
