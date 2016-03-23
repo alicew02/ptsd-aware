b8ee95abd4e8148da9a9bb67d5a9c0c28766eb15
Darius <darius04@vt.edu>
2016-03-23 11:23:42 -0400

    BPM test

--- wear/src/main/java/com/teamcasey/watchdog/BaseliningActivity.java
+++ wear/src/main/java/com/teamcasey/watchdog/BaseliningActivity.java
@@ -1,13 +1,24 @@
 package com.teamcasey.watchdog;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class BaseliningActivity extends Activity {
+public class BaseliningActivity extends Activity implements SensorEventListener {
 
-    private TextView mTextView;
+    private static final String TAG = BaseliningActivity.class.getName();
+
+    private TextView rate;
+    private TextView accuracy;
+    private TextView sensorInformation;
+    private Sensor mHeartRateSensor;
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +28,34 @@ public class BaseliningActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                rate = (TextView) stub.findViewById(R.id.rate);
+                accuracy = (TextView) stub.findViewById(R.id.accuracy);
+                sensorInformation = (TextView) stub.findViewById(R.id.sensor);
             }
         });
+
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.values[0] > 0) {
+            Log.d(TAG, "sensor event: " + event.accuracy + " = " + event.values[0]);
+            rate.setText(String.valueOf(event.values[0]));
+            accuracy.setText("Accuracy: " + event.accuracy);
+            sensorInformation.setText(event.sensor.toString());
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Filler
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSensorManager.registerListener(this, this.mHeartRateSensor, 3);
     }
 }
