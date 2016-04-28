216cb5cd6e8f5e7a0b69265fecec3821bf8e3236
Darius <darius04@vt.edu>
2016-04-28 00:11:23 -0400

    *SHOULD* properly save the user's default selected activity

--- mobile/src/main/java/com/teamcasey/watchdog/TechniquesDashboardActivity.java
+++ mobile/src/main/java/com/teamcasey/watchdog/TechniquesDashboardActivity.java
@@ -1,68 +1,28 @@
 package com.teamcasey.watchdog;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.app.Activity;
+import android.content.SharedPreferences;
+
 
 public class TechniquesDashboardActivity extends Activity {
 
-    public static String packageString = "com.google.android.youtube";
-    public static String filter = "ASMR";
-
-    private int checkedId;
-
-    final RadioButton comedyRadioButton = (RadioButton) findViewById(R.id.comedyRadioButton);
-    final RadioButton asmrRadioButton = (RadioButton) findViewById(R.id.asmrRadioButton);
-    final RadioButton spotifyRadioButton = (RadioButton) findViewById(R.id.spotifyRadioButton);
-    final RadioButton tetrisRadioButton = (RadioButton) findViewById(R.id.tetrisRadioButton);
-
-    RadioGroup techniqueGroup;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_techniques_dashboard);
+        radioGroup = (RadioGroup)findViewById(R.id.techniquesRadioGroup);
+        radioGroup.setOnCheckedChangeListener(radioGroupOnCheckedChangeListener);
 
-        RadioGroup techniqueGroup = (RadioGroup) findViewById(R.id.techniquesRadioGroup);
-
-        techniqueGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int id) {
-                checkedId = id;
-                RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
-
-                if (checkedRadioButton.getText().equals("Comedy Playlist")) {
-                    comedyRadioButton.setChecked(true);
-                    packageString = "com.google.android.youtube";
-                    filter = "Comedy";
-                } else if (checkedRadioButton.getText().equals("ASMR Playlist")) {
-                    asmrRadioButton.setChecked(true);
-                    packageString = "com.google.android.youtube";
-                    filter = "ASMR";
-                } else if (checkedRadioButton.getText().equals("Spotify")) {
-                    spotifyRadioButton.setChecked(true);
-                    packageString = "com.spotify.music";
-                } else if (checkedRadioButton.getText().equals("Tetris")) {
-                    tetrisRadioButton.setChecked(true);
-                    packageString = "com.ea.game.tetris2011_row";
-                }
-                //String text = checkedRadioButton.getText().toString();
-            }
-        });
-        setupButtonListeners();
-    }
-
-    protected void onResume() {
-        super.onResume();
-        RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
-        checkedRadioButton.setChecked(true);
-    }
-
-    private void setupButtonListeners() {
         final Button backButton = (Button) findViewById(R.id.techniquesBackButton);
 
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -70,14 +30,45 @@ public class TechniquesDashboardActivity extends Activity {
                 toDashboard(v);
             }
         });
-    }
 
-    private void toDashboard (View view) {
+        LoadPreferences();
+    }
+    
+    OnCheckedChangeListener radioGroupOnCheckedChangeListener =
+      new OnCheckedChangeListener(){
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+     RadioButton checkedRadioButton = (RadioButton)radioGroup.findViewById(checkedId);
+     int checkedIndex = radioGroup.indexOfChild(checkedRadioButton);
+     String techName = checkedRadioButton.getText().toString();
+     
+     SavePreferences(checkedIndex, techName);
+    }
+      };
+
+ private void SavePreferences(int value, String name) {
+
+     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+     SharedPreferences.Editor preferenceEditor = sharedPreferences.edit();
+
+     preferenceEditor.putString("techName", name);
+     preferenceEditor.putInt("id", value);
+     preferenceEditor.commit();
+ }
+ 
+ private void LoadPreferences(){
+     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+  int savedRadioIndex = sharedPreferences.getInt("id", 0);
+     RadioButton savedCheckedRadioButton = (RadioButton)radioGroup.getChildAt(savedRadioIndex);
+  savedCheckedRadioButton.setChecked(true);
+ }
+
+    private void toDashboard(View view) {
         Intent backIntent = new Intent(this, DashboardActivity.class);
         startActivity(backIntent);
-    }
-
-    private void getID() {
-        //
     }
 }
